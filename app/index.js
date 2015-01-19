316f9367253ccf28b9ad27e0096e805ae0fbcaf1
Ford Lee <fordlee404@gmail.com>
2015-01-19 10:25:20 +0800

    add package.json in app

--- app/index.js
+++ app/index.js
@@ -10,47 +10,114 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   // Where you prompt users for options (where you'd call this.prompt())
-  prompting: function () {
-    var done = this.async();
+  prompting:{
+    welcome: function(){
+      // Have Yeoman greet the user.
+      this.log(yosay(
+        'Welcome to the fantastic ' + chalk.red('Fordlee404') + ' generator!'
+      ));
+    },
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fantastic' + chalk.red('Fordlee404') + ' generator!'
-    ));
+    name: function () {
+      var done = this.async(),
+        root = this.destinationRoot();
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      var prompts = [{
+        type: 'input',
+        name: 'appName',
+        message: 'What is your app name',
+        default: function(){
+          var rootArr = root.split('/'),
+            len = rootArr.length;
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+          return rootArr[len-1];
+        }
+      }];
 
-      done();
-    }.bind(this));
+      this.prompt(prompts, function (props) {
+        this.config.set(props);
+
+        done();
+      }.bind(this));
+    },
+
+    authors: function(){
+      var done = this.async();
+
+      var prompt = [{
+        type: 'input',
+        name: 'authors',
+        message: 'Author: '
+      }];
+
+      this.prompt(prompt, function(props){
+        this.config.set(props);
+
+        done()
+      }.bind(this));
+    },
+
+    description: function(){
+      var done = this.async();
+
+      var prompt = [{
+        type: 'input',
+        name: 'description',
+        message: 'Description: '
+      }];
+
+      this.prompt(prompt, function(props){
+        this.config.set(props);
+
+        done()
+      }.bind(this));
+    },
+
+    license: function(){
+      var done = this.async();
+
+      var prompt = [{
+        type: 'input',
+        name: 'license',
+        message: 'License: ',
+        default: 'MIT'
+      }];
+
+      this.prompt(prompt, function(props){
+        this.config.set(props);
+
+        done()
+      }.bind(this));
+    }
   },
 
   // Saving configurations and configure the project (creating .editorconfig files and other metadata files)
   // configuring: function(){},
 
   // If the method name doesn't match a priority, it is pushed in the default group
-  // default: function(){},
+  // default
 
   // Where you write the generator specific files (routes, controllers, etc)
   writing: {
     app: function () {
-      this.fs.copy(
+      this.fs.copyTpl(
         this.templatePath('_package.json'),
-        this.destinationPath('package.json')
+        this.destinationPath('package.json'),
+        {
+          appName: this.config.get('appName'),
+          authors: this.config.get('authors'),
+          description: this.config.get('description'),
+          license: this.config.get('license')
+        }
       );
+      /*
       this.fs.copy(
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json')
-      );
+      );*/
     },
 
+    /*
     projectfiles: function () {
       this.fs.copy(
         this.templatePath('editorconfig'),
@@ -60,18 +127,19 @@ module.exports = yeoman.generators.Base.extend({
         this.templatePath('jshintrc'),
         this.destinationPath('.jshintrc')
       );
-    }
+    }*/
   },
 
   // Where conflicts are handled (used internally)
   // conflicts: function(){},
 
   // Where installation are run (npm, bower)
+  /*
   install: function () {
     this.installDependencies({
       skipInstall: this.options['skip-install']
     });
-  }
+  }*/
 
   // Called last, cleanup, say good bye, etc
   // end: function(){}
