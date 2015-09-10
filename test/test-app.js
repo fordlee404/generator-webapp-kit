596755116ae239a648c41158e9c2d760c5d5ce6b
Ford Lee <fordlee404@gmail.com>
2015-09-10 16:21:08 +0800

    assert files create

--- test/test-app.js
+++ test/test-app.js
@@ -7,6 +7,39 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('webapp-kit:app', function () {
+  describe('all default input', function(){
+    before(function(done){
+      helpers.run(path.join(__dirname, '../app'))
+        .withOptions({ skipInstall: true })
+        .on('end',done);
+    });
+
+    it('create files', function(){
+      assert.file([
+        'Readme.md',
+        'Gruntfile.coffee',
+        'webpack.config.js',
+        'webpack.production.config.js',
+        'srcHTML/Readme.md',
+        'HTML/Readme.md',
+        'scripts/Readme.md',
+        'fake-response/Readme.md',
+        'images/Readme.md',
+        'plugins/Readme.md',
+        'psd/Readme.md',
+        'stylesheets/Readme.md',
+        'srcHTML/common/_page-head.html',
+        'srcHTML/common/_page-foot.html',
+        'stylesheets/common/util.css',
+        'stylesheets/common/common.css',
+        'srcHTML/index.html',
+        'stylesheets/pages/website-index.css',
+        'scripts/pages/website-index.js',
+        'app-entry.js'
+      ]);
+    });
+  });
+
   describe('when app name have whitespace', function(){
     before(function(done){
       helpers.run(path.join(__dirname, '../app'))
@@ -20,15 +53,6 @@ describe('webapp-kit:app', function () {
     it('change whitespace to middle line in app name', function(){
       var pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
       assert.equal(pkg.name, 'Test-example');
-    });
-
-    it('creates files', function () {
-      assert.file([
-        'bower.json',
-        'package.json',
-        '.editorconfig',
-        '.jshintrc'
-      ]);
     });
   });
 });
